f98e765d2d397319e3adb7ef792a27187f2931b3
sandstormdarude <68867601+sandstormdarude@users.noreply.github.com>
2020-09-11 14:44:42 +0300

    Add files via upload

---
+++ hw4.java
@@ -0,0 +1,9 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class hw4 implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Check");
+    }
+}
